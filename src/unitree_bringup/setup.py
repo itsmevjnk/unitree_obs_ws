3107b4d4aef1c714b888b8d59b7fd9f2b7ba97e7
Vinh Nguyen <ngtv0404@gmail.com>
2025-01-16 15:32:58 +1100

    unitree_bringup revamp, now that 397 has been updated and can thus use unitree_ros
    ekf_filter is not working yet, will probably need more tinkering

--- src/unitree_bringup/setup.py
+++ src/unitree_bringup/setup.py
@@ -12,8 +12,11 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'nav2_params.yaml']),
+        ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*_config.*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'map'), glob(os.path.join('map', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
